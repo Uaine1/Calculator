89f9d99f579d85d9397ad4c20ba55c79ff5e3ced
Uaine1 <angeloalcantara243@gmail.com>
2025-01-15 08:44:42 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Calculator
+
+### Description
+This simple project is here to serve as a python reviewer for me
+
+### Features
+Just like your day to day calculator, it can do;
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+### To do
+Things i wanna with this project.
+- Make a UI
+- Make the calculator be able to handle multiple lines of operations
+- Add other functions
+
---
+++ main.py
@@ -0,0 +1,51 @@
+while True:
+    print("Calculator")
+    user_input = (input("Enter the thing here:"))
+    
+    if "+" in user_input:
+        user_input = user_input.split("+")
+        
+        try:
+            num1 = int(user_input[0].strip())
+            num2 = int(user_input[1].strip())
+            result = num1 + num2
+            print(result)
+            
+        except ValueError:
+            print("Invalid")
+            
+    elif "-" in user_input:
+        user_input = user_input.split("-")
+        
+        try:
+            num1 = int(user_input[0].strip())
+            num2 = int(user_input[1].strip())
+            result = num1 - num2
+            print(result)
+            
+        except ValueError:
+            print("Invalid")
+            
+    elif "x" in user_input:
+        user_input = user_input.split("x")
+        
+        try:
+            num1 = int(user_input[0].strip())
+            num2 = int(user_input[1].strip())
+            result = num1 * num2
+            print(result)
+            
+        except ValueError:
+            print("Invalid")
+            
+    elif "/" in user_input:
+        user_input = user_input.split("/")
+        
+        try:
+            num1 = int(user_input[0].strip())
+            num2 = int(user_input[1].strip())
+            result = num1 / num2
+            print(result)
+            
+        except ValueError:
+            print("Invalid")   
